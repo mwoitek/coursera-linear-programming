17699aa3533d79e8727ee388f45c6e2560d8fc3e
Marcio Woitek <mwoitekjr@gmail.com>
2024-06-30 20:23:48 -0300

    W4 Assignment: Solve Problem 2

--- week_4/assignment_4.py
+++ week_4/assignment_4.py
@@ -20,6 +20,8 @@
 # %%
 from random import randint, uniform
 
+import matplotlib.pyplot as plt
+import networkx as nx
 from pulp import *  # pyright: ignore [reportWildcardImportFromLibrary]
 
 # %% [markdown]
@@ -447,3 +449,175 @@ for trial in range(20):
     print("----------")
 
 print("Test passed: 4 points")
+
+# %% [markdown]
+# ## Problem 2
+
+# %%
+# Write down the cost matrix as a list of lists
+large_number = 10**9
+cost_matrix = [
+    [None, 1, 50, 51, large_number],
+    [1, None, 2, 52, 53],
+    [50, 2, None, 3, 54],
+    [51, 52, 3, None, 4],
+    [large_number, 53, 54, 4, None],
+]
+
+# %% [markdown]
+# The following code is not part of the assignment solution.
+
+# %%
+G = nx.Graph()
+for i in range(4):
+    for j in range(i + 1, 5):
+        G.add_edge(i, j, weight=cost_matrix[i][j])
+
+# %%
+_, ax = plt.subplots(layout="tight")
+pos = nx.shell_layout(G)
+nx.draw_networkx(G, pos, ax=ax)
+edge_labels = {(u, v): G.edges[u, v]["weight"] for u, v in G.edges()}
+nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, ax=ax)
+plt.show()
+
+# %%
+T = nx.minimum_spanning_tree(G)
+
+# %%
+_, ax = plt.subplots(layout="tight")
+pos = nx.shell_layout(T)
+nx.draw_networkx(T, pos, ax=ax)
+edge_labels = {(u, v): T.edges[u, v]["weight"] for u, v in T.edges()}
+nx.draw_networkx_edge_labels(T, pos, edge_labels=edge_labels, ax=ax)
+plt.show()
+
+# %% [markdown]
+# Back to the assignment code:
+
+# %%
+# Check that the cost matrix is symmetric
+assert len(cost_matrix) == 5, f"Cost matrix must have 5 rows. Yours has {len(cost_matrix)} rows."
+assert all(len(row) == 5 for row in cost_matrix), f"Each row of the cost matrix must have 5 entries."
+for i in range(5):
+    for j in range(i):
+        assert (
+            cost_matrix[i][j] == cost_matrix[j][i]
+        ), f"Cost matrix fails to be symmetric at entries {(i, j)} and {(j, i)}"
+print("Structure of your cost matrix looks OK (3 points).")
+
+
+# %%
+# MST based TSP approximation
+# This code implements the simple MST based shortcutting approach that would
+# yield factor of 2 approximation for metric TSPs
+def minimum_spanning_tree_tsp(n, cost_matrix):
+    G = nx.Graph()
+    for i in range(n):
+        for j in range(i):
+            G.add_edge(i, j, weight=cost_matrix[i][j])
+    T = nx.minimum_spanning_tree(G)
+    print(f"MST for your graph has the edges {T.edges}")
+    mst_cost = 0
+    mst_dict = {}  # Store MST as a dictionary
+    for i, j in T.edges:
+        mst_cost += cost_matrix[i][j]
+        if i in mst_dict:
+            mst_dict[i].append(j)
+        else:
+            mst_dict[i] = [j]
+        if j in mst_dict:
+            mst_dict[j].append(i)
+        else:
+            mst_dict[j] = [i]
+    print(f"MST cost: {mst_cost}")
+    print(mst_dict)
+
+    # Form a tour with shortcutting
+    def traverse_mst(tour_so_far, cur_node):
+        assert cur_node in mst_dict
+        next_nodes = mst_dict[cur_node]
+        for j in next_nodes:
+            if j in tour_so_far:
+                continue
+            tour_so_far.append(j)
+            traverse_mst(tour_so_far, j)
+
+    tour = [0]
+    traverse_mst(tour, 0)
+    i = 0
+    tour_cost = 0
+    for j in tour[1:]:
+        tour_cost += cost_matrix[i][j]
+        i = j
+    tour_cost += cost_matrix[i][0]
+
+    return tour, tour_cost
+
+
+# %%
+# Optimal TSP tour using MTZ encoding
+def mtz_encoding_tsp(n, cost_matrix):
+    assert len(cost_matrix) == n, f"Cost matrix is not {n}x{n}"
+    assert all(len(row) == n for row in cost_matrix), f"Cost matrix is not {n}x{n}"
+    # Create encoding variables
+    binary_vars = [
+        [LpVariable(f"x_{i}_{j}", cat="Binary") if i != j else None for j in range(n)] for i in range(n)
+    ]
+    # Add time stamps
+    time_stamps = [LpVariable(f"t_{i}", lowBound=0, upBound=n) for i in range(1, n)]
+    # Create the problem
+    prob = LpProblem("TSP-MTZ", LpMinimize)
+    # Add the objective function
+    prob += lpSum(
+        lpSum(xij * cj if xij is not None else 0 for xij, cj in zip(brow, crow))
+        for brow, crow in zip(binary_vars, cost_matrix)
+    )
+    # Add the degree constraints
+    for i in range(n):
+        prob += lpSum(xj for xj in binary_vars[i] if xj is not None) == 1
+        prob += lpSum(binary_vars[j][i] for j in range(n) if j != i) == 1
+    # Add time stamp constraints
+    for i in range(1, n):
+        for j in range(1, n):
+            if i == j:
+                continue
+            xij = binary_vars[i][j]
+            ti = time_stamps[i - 1]
+            tj = time_stamps[j - 1]
+            prob += tj >= ti + xij - (1 - xij) * (n + 1)  # pyright: ignore
+    # Solve the problem
+    status = prob.solve(PULP_CBC_CMD(msg=False))
+    assert status == constants.LpStatusOptimal, f"Unexpected non-optimal status {status}"
+    # Extract the tour
+    tour = [0]
+    tour_cost = 0
+    while len(tour) < n:
+        i = tour[-1]
+        sols = [j for j, xij in enumerate(binary_vars[i]) if xij is not None and xij.varValue >= 0.999]  # pyright: ignore
+        assert len(sols) == 1
+        j = sols[0]
+        tour_cost += cost_matrix[i][j]
+        tour.append(j)
+        assert j != 0
+    i = tour[-1]
+    tour_cost += cost_matrix[i][0]
+    return tour, tour_cost
+
+
+# %%
+# Test that exact answer is 10^6 times smaller than approximate answer
+# Compute MST based approximation
+tour, tour_cost = minimum_spanning_tree_tsp(5, cost_matrix)
+print(f"MST approximation yields tour {tour} with cost {tour_cost}")
+
+# Compute exact answer
+opt_tour, opt_tour_cost = mtz_encoding_tsp(5, cost_matrix)
+print(f"Optimal tour is {opt_tour} with cost {opt_tour_cost}")
+
+# Check that the fraction is 1 million times apart
+assert (
+    tour_cost / opt_tour_cost >= 1e06
+), f"The TSP + shortcutting tour must be at least 10^6 times costlier than optimum. In your case, the ratio is {tour_cost / opt_tour_cost}"
+
+print("Test passed: 7 points")
